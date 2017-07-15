32ff75031de259531773318f6e2e3437ad3a87f5
Sean Quigley <seanq@giphy.com>
2017-07-15 15:54:47 -0400

    setup basic Akka HTTP server and routes

--- build.sbt
+++ build.sbt
@@ -4,5 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
+val akkaHttpVersion = "10.0.9"
+
 libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 )
--- src/main/scala/org/seanpquig/mini/search/Service.scala
+++ src/main/scala/org/seanpquig/mini/search/Service.scala
@@ -1,9 +1,38 @@
 package org.seanpquig.mini.search
 
+import scala.concurrent.ExecutionContext
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+
+
 object Service {
 
+  val interface = "::0"
+  val port = 8080
+
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  implicit val ec: ExecutionContext = system.dispatcher
+
+  def routes: Route = {
+    pathSingleSlash {
+      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Mini Search</h1>"))
+    } ~
+    path("search") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    println("STUB")
+    Http().bindAndHandle(routes, interface, port)
+    println(s"MiniSearch Server running at http://localhost:$port")
   }
 
 }
