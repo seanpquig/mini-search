b523149a10f698bae3c91e7543f22e0ece44f21b
Sean Quigley <seanpquig@gmail.com>
2018-12-28 01:35:57 -0500

    refactor ignoreTrailingSlash

--- src/main/scala/org/seanpquig/mini/search/Routes.scala
+++ src/main/scala/org/seanpquig/mini/search/Routes.scala
@@ -10,45 +10,41 @@ object Routes extends JsonSupport {
 
   val logger = Logger("mini-search-routes")
 
-  def all: Route = {
+  def all: Route = ignoreTrailingSlash {
     pathSingleSlash {
       complete("Mini Search")
     } ~ searchRoutes ~ indexingRoutes
   }
 
-  def searchRoutes: Route = ignoreTrailingSlash {
-    path("search" / Segment) { idxName =>
-      post {
-        entity(as[SearchRequest]) { request =>
-          // Attempt to search against index
-          val docsOpt = MiniSearch.search(idxName, request.query)
-          val response = docsOpt.map(_.take(request.limit))
-            .map(SearchResponse(s"Successful search against $idxName", _))
-            .getOrElse(SearchResponse(s"Index $idxName does not exist.", List()))
+  def searchRoutes: Route = path("search" / Segment) { idxName =>
+    post {
+      entity(as[SearchRequest]) { request =>
+        // Attempt to search against index
+        val docsOpt = MiniSearch.search(idxName, request.query)
+        val response = docsOpt.map(_.take(request.limit))
+          .map(SearchResponse(s"Successful search against $idxName", _))
+          .getOrElse(SearchResponse(s"Index $idxName does not exist.", List()))
 
-          complete(response)
-        }
+        complete(response)
       }
     }
   }
 
   private val imgTagger = new InceptionVisionTagger(Config.modelPath)
 
-  def indexingRoutes: Route = ignoreTrailingSlash {
-    path("index" / Segment) { idxName =>
-      get {
-        complete(s"Index info for $idxName")
-      } ~
-      post {
-        entity(as[IndexRequest]) { request =>
-          complete(s"Create index: $idxName")
-        }
+  def indexingRoutes: Route = path("index" / Segment) { idxName =>
+    get {
+      complete(s"Index info for $idxName")
+    } ~
+    post {
+      entity(as[IndexRequest]) { request =>
+        complete(s"Create index: $idxName")
       }
-    } ~ path("indices") {
-      complete(MiniSearch.indicesResponse())
-    } ~ path("vision_upload") {
-      complete(imgTagger.imageToTags("/Users/seanq/Downloads/cat.jpg"))
     }
+  } ~ path("indices") {
+    complete(MiniSearch.indicesResponse())
+  } ~ path("vision_upload") {
+    complete(imgTagger.imageToTags("/Users/seanq/Downloads/cat.jpg"))
   }
 
 }
